e4ba9a8323e666cdbe56a4d921fc61696a8d2bb0
Marcus Silveira <marcusvinicius.767.mv@gmaiil.com>
2023-10-19 10:41:06 -0300

    Configurado banco de dados e home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+// Health Ckech
+[ApiController]
+[Route("")]
+public class HomeController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult Get([FromServices] BlogDataContext blogDataContext)
+    {
+        var categories = blogDataContext.Users.ToList();
+        return Ok(categories);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Blog.Data;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+DotNetEnv.Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
+
+// var connectionString = builder.Configuration.GetConnectionString("SqlServer");
+var dbConnectionString = Env.GetString("SqlServer");
+
+builder.Services.AddDbContext<BlogDataContext>(options => options.UseSqlServer(dbConnectionString));
+builder.Services.AddControllers();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
 app.Run();
